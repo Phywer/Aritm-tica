2aacb36deca9f2c861c7f8e1bd7f9aa321080ca8
David Sáez Faus <al375914@aula1152act.sg.uji.es>
2019-01-29 14:20:02 +0100

    Before puesto

--- src/test/java/AritmeticaTest.java
+++ src/test/java/AritmeticaTest.java
@@ -1,31 +1,39 @@
 import aritmetica.Aritmetica;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AritmeticaTest {
+    private Aritmetica aritmetica;
+
+    @Before
+    public void inicio(){
+        System.out.println("Método inicio");
+        aritmetica= new Aritmetica();
+    }
+
     @Test
     public void sumaTest(){
-        //fail("Esta prueba va a fallar");
-        Aritmetica aritmetica = new Aritmetica();
+        System.out.println("Método sumatest");
         assertEquals(2,aritmetica.suma(1,1),0);
     }
 
 
     @Test
-    public void restatest(){
-        Aritmetica aritmerica = new Aritmetica();
-        assertEquals(0,aritmerica.resta(1,1),0);
+    public void restaTest(){
+        System.out.println("Método restaTest");
+        assertEquals(0,aritmetica.resta(1,1),0);
     }
 
     @Test
     public void testMultiplicacion() {
-        Aritmetica aritmetica = new Aritmetica();
+        System.out.println("Método testMultiplicacion");
         assertEquals(6, aritmetica.multiplicacion(2, 3), 0);
     }
     @Test
     public void testDivision() {
-        Aritmetica aritmetica = new Aritmetica();
+        System.out.println("Método testDivision");
         assertEquals(5, aritmetica.division(10, 2), 0);
     }
 }
